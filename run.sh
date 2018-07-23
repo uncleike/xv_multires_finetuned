2ecdb14ac6daa30d83c9d2ca0f4bf46bf92b239c
uncleike <41551900+uncleike@users.noreply.github.com>
2018-07-23 09:02:14 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ OUTPUT_DIR=$2
 
 OUTPUT_FILE = ${OUTPUT_DIR}/$(basename $VALIDATE_FILE).txt
 
-python create_detections_1.py -c ./multiresfinetuned5296.pb --input $VALIDATE_FILE --output $OUTPUT_FILE
+python create_detections_1.py -c ./model.pb --input $VALIDATE_FILE --output $OUTPUT_FILE
