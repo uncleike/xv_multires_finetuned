78c580d36bd502e2c09d0be4d90c2d82960e7037
uncleike <41551900+uncleike@users.noreply.github.com>
2018-07-24 10:25:45 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ set -euxo pipefail
 VALIDATE_FILE=$1
 OUTPUT_DIR=$2
 
-OUTPUT_FILE = ${OUTPUT_DIR}/$(basename $VALIDATE_FILE).txt
+OUTPUT_FILE =${OUTPUT_DIR}/$(basename $VALIDATE_FILE).txt
 
 python create_detections_1.py -c ./model.pb --input $VALIDATE_FILE --output $OUTPUT_FILE
